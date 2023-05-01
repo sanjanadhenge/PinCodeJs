76f7347d2578eee1722e8eb9129c69413e3191de
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-05-01 12:59:37 +0530

    [sanjana] Add :EmailTest

--- Email.js
+++ Email.js
@@ -1,6 +1,8 @@
 let EMAIL_Regex = RegExp('^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}');
 let sample = "abc.xyz@Brigelabz.co.in";
+ arr = [ "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", " abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc..2002@gmail.com", "abc.@gmail.com", "bc@abc@gmail.com", "abc@%*.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" ];
 ValidatePincode(sample,EMAIL_Regex);
+ValidateArray();
 function ValidatePincode(email,EMAIL_Regex)
 {
     if(EMAIL_Regex.test(email))
@@ -11,4 +13,19 @@ function ValidatePincode(email,EMAIL_Regex)
     {
         console.log("Invalid");
     }
+}
+function ValidateArray()
+{
+    for (let i = 0; i < arr.length; i++)
+    {
+         let word = arr[i];
+         if (EMAIL_Regex.test(word))
+         {
+             console.log("Valid----->" + word);
+         }
+         else
+         {
+             console.log("Invalid------>" + word);
+         }
+     }
 }
