08cb0607fc3b6bc86fe058284d2e3a1eaab33515
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-05-01 12:39:19 +0530

    [sanjana] Add :Validate .Co

--- Email.js
+++ Email.js
@@ -1,5 +1,5 @@
-let EMAIL_Regex =  RegExp('^[0-9a-zA-Z]+[@][a-zA-Z]');
-let sample = "abc@bridgelabz";
+let EMAIL_Regex = RegExp('^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}');
+let sample = "abc.xyz@Brigelabz.co";
 ValidatePincode(sample,EMAIL_Regex);
 function ValidatePincode(email,EMAIL_Regex)
 {
